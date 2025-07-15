20823c184236385a7b9bb9a9216ac394b71ded94
SrLLamaDev <srllamadev@gmail.com>
2025-07-15 11:01:39 -0400

    Add turtle graphics example to draw an isosceles triangle

---
+++ 0_Turtle_Ejercicios/ex_1_turtle_v1.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle()
+t.pensize(3)  # Configurar el grosor del pincel
+t.speed(3)
+# Dibujar triángulo isósceles
+t.forward(100)  # Base del triángulo
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+turtle.done()
