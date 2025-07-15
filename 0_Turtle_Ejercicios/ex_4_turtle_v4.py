0d837d68cf3f0abdda62413cabbccd10a6dc0d67
SrLLamaDev <srllamadev@gmail.com>
2025-07-15 11:02:45 -0400

    Add turtle graphics example to draw the entire alphabet

---
+++ 0_Turtle_Ejercicios/ex_4_turtle_v4.py
@@ -0,0 +1,632 @@
+#ALfabhet of Fahim Arman ty
+import turtle
+
+def A():
+    turtle.left(75)
+    turtle.forward(103.5)
+    
+    turtle.right(150)
+    turtle.forward(103.5)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(52)
+    turtle.left(75)
+    turtle.pendown()
+    turtle.forward(26.65)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(26.65)
+    turtle.right(75)
+    turtle.forward(52)
+    turtle.left(75)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def B():
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.right(90)
+    turtle.forward(25)
+    
+    turtle.circle(-25, 180)
+    turtle.forward(25)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(25)
+    turtle.pendown()
+    turtle.circle(-25,180)
+    turtle.forward(25)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(50)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def C():
+    turtle.penup()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(75)
+    turtle.pendown()
+    
+    turtle.circle(25,180)
+    turtle.forward(50)
+    turtle.circle(25,180)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(25)
+    turtle.left(90)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def D():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.circle(-50,180)
+    turtle.forward(10)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(60)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def E():
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.right(90)
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.pendown()
+    
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.pendown()
+    
+    turtle.left(90)
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.forward(10)
+    turtle.pendown()
+    
+def F():
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.right(90)
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.pendown()
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def G():
+    turtle.penup()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(75)
+    turtle.pendown()
+    
+    turtle.circle(25,180)
+    turtle.forward(50)
+    turtle.circle(25,180)
+    
+    turtle.left(90)
+    turtle.forward(25)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(25)
+    turtle.pendown()
+    
+    turtle.right(90)
+    turtle.forward(25)
+    
+    turtle.penup()
+    turtle.left(90)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def H():
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.pendown()
+    turtle.forward(50)
+    
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(180)
+    turtle.forward(100)
+    
+    turtle.penup()
+    turtle.left(90)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def I():
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(100)
+    turtle.left(90)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def J():
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.pendown()
+    
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(75)
+    turtle.circle(-25,180)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(25)
+    turtle.left(90)
+    turtle.forward(50)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def K():
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(50)    
+    turtle.pendown()
+    
+    turtle.left(135)
+    turtle.forward(70.71)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(70.71)
+    turtle.pendown()
+    
+    turtle.left(90)
+    turtle.forward(70.71)
+    
+    turtle.penup()
+    turtle.left(45)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def L():
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(100)
+    turtle.pendown()
+    
+    turtle.left(90)
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.forward(10)
+    turtle.pendown()
+    
+    
+def M():
+    turtle.left(75)
+    turtle.forward(103.5)
+    
+    turtle.right(150)
+    turtle.forward(103.5)
+    
+    
+    turtle.left(150)
+    turtle.forward(103.5)
+    
+    turtle.right(150)
+    turtle.forward(103.5)
+    
+    turtle.penup()
+    turtle.left(75)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def N():
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.right(150)
+    turtle.forward(115.47)
+    
+    turtle.left(150)
+    turtle.forward(100)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(100)
+    
+    turtle.left(90)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+    
+def O():
+    turtle.penup()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(75)
+    turtle.pendown()
+    
+    turtle.circle(25,180)
+    turtle.forward(50)
+    turtle.circle(25,180)
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(75)
+    turtle.left(90)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def P():
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.right(90)
+    turtle.forward(25)
+    
+    turtle.circle(-25, 180)
+    turtle.forward(25)
+    
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(50)
+    
+    turtle.left(90)
+    turtle.forward(50)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def Q():
+    turtle.penup()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(75)
+    turtle.pendown()
+    
+    turtle.circle(25,180)
+    turtle.forward(50)
+    turtle.circle(25,180)
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(75)
+    
+    
+    turtle.left(225)
+    turtle.forward(25)
+    turtle.pendown()
+    
+    turtle.left(180)
+    turtle.forward(25)
+    
+    turtle.penup()
+    turtle.left(45)
+    turtle.forward(10)
+    turtle.pendown()
+
+def R():
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.right(90)
+    turtle.forward(25)
+    
+    turtle.circle(-25, 180)
+    turtle.forward(25)
+    
+    turtle.left(135)
+    turtle.forward(70.71)
+    
+    turtle.penup()
+    turtle.left(45)
+    turtle.forward(10)
+    turtle.pendown()
+
+def S():
+    turtle.penup()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(75)
+    turtle.pendown()
+    
+    turtle.circle(25,180)
+    turtle.left(45)
+    turtle.forward(70.71)
+    turtle.right(45)
+    turtle.circle(-25,180)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(25)
+    turtle.left(90)
+    turtle.forward(50)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def T():  
+    turtle.penup()
+    turtle.forward(25)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(25)
+    turtle.left(180)
+    turtle.pendown()
+    
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(25)
+    turtle.pendown()
+    
+    turtle.left(90)
+    turtle.forward(100)
+    
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(25)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+  
+def U():
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.pendown()
+    
+    turtle.left(180)
+    turtle.forward(75)
+    turtle.circle(25,180)
+    turtle.forward(75)
+    
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(100)
+    turtle.left(90)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def V():
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.pendown()
+    
+    turtle.right(165)
+    turtle.forward(103.5)
+    turtle.left(150)
+    turtle.forward(103.5)
+    
+    turtle.penup()
+    turtle.right(165)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(10)
+    turtle.pendown()
+    
+def W():
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.pendown()
+    
+    turtle.right(165)
+    turtle.forward(103.5)
+    turtle.left(150)
+    turtle.forward(103.5)
+    
+    turtle.right(150)
+    turtle.forward(103.5)
+    turtle.left(150)
+    turtle.forward(103.5)
+    
+    turtle.penup()
+    turtle.right(165)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(10)
+    turtle.pendown()
+    
+def X():
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.pendown()
+    
+    turtle.right(90)
+    turtle.right(63.435)
+    turtle.forward(111.8)
+    
+    turtle.penup()
+    turtle.left(63.435)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.pendown()
+    
+    turtle.left(90)
+    turtle.left(63.435)
+    turtle.forward(111.8)
+    
+    turtle.penup()
+    turtle.right(63.435)
+    turtle.right(180)
+    turtle.forward(50)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def Y():
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.pendown()
+    
+    turtle.right(90)
+    turtle.right(63.435)
+    turtle.forward(55.902)
+    
+    turtle.penup()
+    turtle.left(63.435)
+    turtle.left(63.435)
+    turtle.forward(55.902)
+    turtle.pendown()
+    
+    turtle.left(180)
+    turtle.forward(55.902)
+    
+    turtle.left(90-63.435)
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(25)
+    
+    turtle.forward(10)
+    turtle.pendown()
+    
+def Z():
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.pendown()
+    
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90+90-63.435)
+    turtle.forward(111.8)
+    
+    turtle.left(90+90-63.435)
+    turtle.forward(50)
+    
+    turtle.penup()
+    turtle.forward(10)
+    turtle.pendown()
+    
+    
+turtle.setup(1100,900)
+turtle.bgcolor('yellow')
+turtle.color("red")
+turtle.speed(7)
+
+turtle.penup()
+turtle.goto(-300, 300)
+turtle.pendown()
+turtle.pensize(5)  # Configurar el grosor del pincel
+turtle.speed(100)   # Velocidad de dibujo
+A() 
+B()
+C()
+D()
+E()
+F()
+G()
+H()
+I()
+J()
+
+turtle.penup()
+turtle.goto(-300, 100)
+turtle.pendown()
+
+K()
+L()
+M()
+N()
+O()
+P()
+Q()
+R()
+
+turtle.penup()
+turtle.goto(-300, -100)
+turtle.pendown()
+
+S()
+T()
+U()
+V()
+W()
+X()
+Y()
+Z()
+
+turtle.exitonclick()
+turtle.done()
+turtle.bye()
