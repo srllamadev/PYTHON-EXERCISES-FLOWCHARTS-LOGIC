af0126551b737de77339b52ee680171909e5157c
SrLLamaDev <srllamadev@gmail.com>
2025-07-16 18:16:09 -0400

    Add turtle graphics implementation for various shapes including triangle, square, rectangle, circle, rhombus, and ellipse

---
+++ 0_Turtle_Ejercicios/Example_Landscape.py
@@ -0,0 +1,691 @@
+#Ejercicio 5
+#Obra de arte :v (ojalá)
+import turtle
+v = turtle.Screen()
+v.bgcolor("skyblue")
+#Montañas grandes
+
+#Fig b - Triangulo
+b = turtle.Turtle()
+b.color("#800080")
+b.pensize(3)
+#Acomodar
+b.speed(700)
+#Triangulo equilatero
+b.fillcolor("#FF00FF")
+b.begin_fill()
+b.bk(100)
+b.lt(60)
+b.fd(100)
+b.bk(100)
+b.right(60)
+b.fd(100)
+b.lt(120)
+b.fd(100)
+b.lt(120)#No se porque
+b.fd(100)#No se por que :v
+b.end_fill()
+#------------------------------
+#Fig b1 - Triangulo
+b1 = turtle.Turtle()
+b1.color("#800080")
+b1.pensize(3)
+#Acomodar
+b1.up()
+b1.fd(10)
+b1.down()
+#Triangulo equilatero
+b.speed(700)
+b1.speed(700)
+b1.fillcolor("#FF00FF")
+b1.begin_fill()
+b1.bk(100)
+b1.lt(60)
+b1.fd(100)
+b1.bk(100)
+b1.right(60)
+b1.fd(100)
+b1.lt(120)
+b1.fd(100)
+b1.lt(120)#No se porque
+b1.fd(100)#No se por que :v
+b1.end_fill()
+#------------------------------
+#Fig b2 - Triangulo
+b2 = turtle.Turtle()
+b2.speed(700)
+
+b2.color("#800080")
+b2.pensize(3)
+#Acomodar
+b2.up()
+b2.fd(20)
+b2.down()
+#Triangulo equilatero
+b2.fillcolor("#FF00FF")
+b2.begin_fill()
+b2.bk(100)
+b2.lt(60)
+b2.fd(100)
+b2.bk(100)
+b2.right(60)
+b2.fd(100)
+b2.lt(120)
+b2.fd(100)
+b2.lt(120)#No se porque
+b2.fd(100)#No se por que :v
+b2.end_fill()
+#------------------------------
+#Fig b3 - Triangulo
+b3 = turtle.Turtle()
+b3.speed(700)
+
+b3.color("#800080")
+b3.pensize(3)
+#Acomodar
+b3.up()
+b3.fd(60)
+b3.down()
+#Triangulo equilatero
+b3.fillcolor("#FF00FF")
+b3.begin_fill()
+b3.bk(100)
+b3.lt(60)
+b3.fd(100)
+b3.bk(100)
+b3.right(60)
+b3.fd(100)
+b3.lt(120)
+b3.fd(100)
+b3.lt(120)#No se porque
+b3.fd(100)#No se por que :v
+b3.end_fill()
+#-----------------------------
+#Montañas
+#Fig 1 - Triangulo
+a = turtle.Turtle()
+a.speed(700)
+
+a.color("#800080")
+a.pensize(3)
+#Acomodar
+
+#Triangulo equilatero
+a.fillcolor("#FF00FF")
+a.begin_fill()
+a.bk(50)
+a.lt(60)
+a.fd(50)
+a.bk(50)
+a.right(60)
+a.fd(50)
+a.lt(120)
+a.fd(50)
+a.lt(120)#No se porque
+a.fd(50)#No se por que :v
+a.end_fill()
+#-------------------------
+#Fig 2 - Triangulo
+a2 = turtle.Turtle()
+a2.speed(700)
+
+a2.color("#800080")
+a2.pensize(3)
+#Acomodar
+a2.up()
+a2.fd(10)
+a2.down()
+#Triangulo equilatero
+a2.fillcolor("#FF00FF")
+a2.begin_fill()
+a2.bk(50)
+a2.lt(60)
+a2.fd(50)
+a2.bk(50)
+a2.right(60)
+a2.fd(50)
+a2.lt(120)
+a2.fd(50)
+a2.lt(120)#No se porque
+a2.fd(50)#No se por que :v
+a2.end_fill()
+#----------------------------------
+#Fig 3 - Triangulo
+a3 = turtle.Turtle()
+a3.color("#800080")
+a3.pensize(3)
+#Acomodar
+a3.up()
+a3.fd(20)
+a3.down()
+#Triangulo equilatero
+a3.fillcolor("#FF00FF")
+a3.begin_fill()
+a3.bk(50)
+a3.lt(60)
+a3.fd(50)
+a3.bk(50)
+a3.right(60)
+a3.fd(50)
+a3.lt(120)
+a3.fd(50)
+a3.lt(120)#No se porque
+a3.fd(50)#No se por que :v
+a3.end_fill()
+#---------------------------------
+#Fig 4 - Triangulo
+a4 = turtle.Turtle()
+a4.color("#800080")
+a4.pensize(3)
+#Acomodar
+a4.up()
+a4.fd(20)
+a4.down()
+#Triangulo equilatero
+a4.fillcolor("#FF00FF")
+a4.begin_fill()
+a4.bk(50)
+a4.lt(60)
+a4.fd(50)
+a4.bk(50)
+a4.right(60)
+a4.fd(50)
+a4.lt(120)
+a4.fd(50)
+a4.lt(120)#No se porque
+a4.fd(50)#No se por que :v
+a4.end_fill()
+#---------------------------------
+#Fig 5 - Triangulo
+a5 = turtle.Turtle()
+a5.color("#800080")
+a5.pensize(3)
+#Acomodar
+a5.up()
+a5.fd(50)
+a5.down()
+#Triangulo equilatero
+a5.fillcolor("#FF00FF")
+a5.begin_fill()
+a5.bk(50)
+a5.lt(60)
+a5.fd(50)
+a5.bk(50)
+a5.right(60)
+a5.fd(50)
+a5.lt(120)
+a5.fd(50)
+a5.lt(120)#No se porque
+a5.fd(50)#No se por que :v
+a5.end_fill()
+#---------------------------------
+#Fig 6 - Triangulo
+a6 = turtle.Turtle()
+a6.color("#800080")
+a6.pensize(3)
+#Acomodar
+a6.up()
+a6.fd(60)
+a6.down()
+#Triangulo equilatero
+a6.fillcolor("#FF00FF")
+a6.begin_fill()
+a6.bk(50)
+a6.lt(60)
+a6.fd(50)
+a6.bk(50)
+a6.right(60)
+a6.fd(50)
+a6.lt(120)
+a6.fd(50)
+a6.lt(120)#No se porque
+a6.fd(50)#No se por que :v
+a6.end_fill()
+#---------------------------------
+#Fig 7 - Triangulo
+a7 = turtle.Turtle()
+a7.color("#800080")
+a7.pensize(3)
+#Acomodar
+a7.up()
+a7.fd(70)
+a7.down()
+#Triangulo equilatero
+a7.fillcolor("#FF00FF")
+a7.begin_fill()
+a7.bk(50)
+a7.lt(60)
+a7.fd(50)
+a7.bk(50)
+a7.right(60)
+a7.fd(50)
+a7.lt(120)
+a7.fd(50)
+a7.lt(120)#No se porque
+a7.fd(50)#No se por que :v
+a7.end_fill()
+#---------------------------------
+#Fig 2 - Triangulo
+a8 = turtle.Turtle()
+a8.color("#800080")
+a8.pensize(3)
+#Acomodar
+a8.up()
+a8.fd(80)
+a8.down()
+#Triangulo equilatero
+a8.fillcolor("#FF00FF")
+a8.begin_fill()
+a8.bk(50)
+a8.lt(60)
+a8.fd(50)
+a8.bk(50)
+a8.right(60)
+a8.fd(50)
+a8.lt(120)
+a8.fd(50)
+a8.lt(120)#No se porque
+a8.fd(50)#No se por que :v
+a8.end_fill()
+#---------------------------------
+#Fig 9 - Triangulo
+a9 = turtle.Turtle()
+a9.color("#800080")
+a9.pensize(3)
+#Acomodar
+a9.up()
+a9.fd(90)
+a9.down()
+#Triangulo equilatero
+a9.fillcolor("#FF00FF")
+a9.begin_fill()
+a9.bk(50)
+a9.lt(60)
+a9.fd(50)
+a9.bk(50)
+a9.right(60)
+a9.fd(50)
+a9.lt(120)
+a9.fd(50)
+a9.lt(120)#No se porque
+a9.fd(50)#No se por que :v
+a9.end_fill()
+#---------------------------------
+#Fig 2 - Triangulo
+a3 = turtle.Turtle()
+a3.color("#800080")
+a3.pensize(3)
+#Acomodar
+a3.up()
+a3.fd(20)
+a3.down()
+#Triangulo equilatero
+a3.fillcolor("#FF00FF")
+a3.begin_fill()
+a3.bk(50)
+a3.lt(60)
+a3.fd(50)
+a3.bk(50)
+a3.right(60)
+a3.fd(50)
+a3.lt(120)
+a3.fd(50)
+a3.lt(120)#No se porque
+a3.fd(50)#No se por que :v
+a3.end_fill()
+#---------------------------------
+#Fig 2 - Triangulo
+a3 = turtle.Turtle()
+a3.color("#800080")
+a3.pensize(3)
+#Acomodar
+a3.up()
+a3.fd(25)
+a3.down()
+#Triangulo equilatero
+a3.fillcolor("#FF00FF")
+a3.begin_fill()
+a3.bk(25)
+a3.lt(60)
+a3.fd(25)
+a3.bk(25)
+a3.right(60)
+a3.fd(25)
+a3.lt(120)
+a3.fd(25)
+a3.lt(120)#No se porque
+a3.fd(25)#No se por que :v
+a3.end_fill()
+#Fig 3 - Plano
+c = turtle.Turtle()
+#c.color("#2630F4")
+c.pensize(3)
+#Acomodar
+
+#Triangulo equilatero
+#c.fillcolor("F42689")
+c.begin_fill()
+c.fd(200)
+c.right(90)
+c.fd(30)
+c.right(90)
+c.fd(400)
+c.right(90)
+c.fd(30)
+c.right(90)
+c.fd(400)
+c.right(90)
+
+c.end_fill()
+
+#Fig c1 - Plano
+c1 = turtle.Turtle()
+c1.color("#808080")
+c1.pensize(3)
+#Acomodar
+c1.up()
+c1.fd(10)
+c1.right(90)
+c1.fd(10)
+c1.lt(90)
+c1.down()
+#Triangulo equilatero
+c1.fillcolor("#C0C0C0")
+c1.begin_fill()
+c1.fd(200)
+c1.right(90)
+c1.fd(30)
+c1.right(90)
+c1.fd(400)
+c1.right(90)
+c1.fd(30)
+c1.right(90)
+c1.fd(400)
+c1.right(90)
+
+c1.end_fill()
+
+#Luna llena
+#Figura cirulo
+g = turtle.Turtle()
+g.shape("turtle")
+#Acomodar
+g.up()
+g.lt(90)
+g.fd(110)
+g.down()
+#Circulo
+g.color("#000080")
+g.fillcolor("#00FFFF")
+g.shape("circle")
+g.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+
+'''
+Nube 1
+'''
+#Nubes
+
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(80)
+n.lt(90)
+n.fd(90)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#--
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(90)
+n.lt(90)
+n.fd(90)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#--
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(100)
+n.lt(90)
+n.fd(90)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#--
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(110)
+n.lt(90)
+n.fd(90)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#---
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(90)
+n.lt(90)
+n.fd(100)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#--
+n = turtle.Turtle()
+n.shape("turtle")
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(100)
+n.lt(90)
+n.fd(100)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#--
+n = turtle.Turtle()
+n.shape("turtle")
+#-----------------------------------
+#----------------------------------
+'''
+Nube 2
+'''
+#Nubes
+
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(160)
+n.lt(90)
+n.fd(90)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#--
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(170)
+n.lt(90)
+n.fd(90)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#--
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(180)
+n.lt(90)
+n.fd(90)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#--
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(190)
+n.lt(90)
+n.fd(90)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#---
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(170)
+n.lt(90)
+n.fd(100)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#--
+n = turtle.Turtle()
+n.shape("turtle")
+n = turtle.Turtle()
+n.shape("turtle")
+#Acomodar
+n.up()
+n.fd(180)
+n.lt(90)
+n.fd(100)
+n.down()
+#Circulo
+n.color("#00FFFF")
+n.fillcolor("white")
+n.shape("circle")
+n.shapesize(1,1,3) #(Ancho,Alto,Grosor)
+#--
+n = turtle.Turtle()
+n.shape("turtle")
+
+#---------------------------------
+#---------------------------------
+'''
+Cohete
+'''
+
+#cuerpo de la nave
+j = turtle.Turtle()
+j.color("#000000")
+j.pensize("3")
+#Acomodar
+j.up()
+j.fd(47)
+j.lt(90)
+j.fd(90)
+j.down()
+#Cuerpo
+j.fillcolor("#FF0000")
+j.begin_fill()
+j.fd(25)
+j.right(90)
+j.fd(10)
+j.right(90)
+j.fd(25)
+j.right(90)
+j.fd(10)
+j.end_fill()
+
+#-----------------------
+#cabeza de la nave
+h = turtle.Turtle()
+h.color("#000000")
+h.pensize("3")
+#Acomodar
+h.up()
+h.fd(47)
+h.lt(90)
+h.fd(115)
+h.down()
+#Cabeza
+h.fillcolor("#000080")
+h.begin_fill()
+h.right(90)
+h.lt(60)
+h.fd(10)
+h.bk(10)
+h.right(60)
+h.fd(10)
+h.lt(120)
+h.fd(10)
+h.lt(120)
+h.fd(10)
+h.lt(120)
+h.fd(10)
+h.end_fill()
+#---------------------------
+#Propuslsión
+p = turtle.Turtle()
+p.color("#FFFF00")
+p.pensize("3")
+#Acomodar
+p.up()
+p.fd(47)
+p.lt(90)
+p.fd(90)
+p.down()
+#Cabeza
+p.fillcolor("#FFFF00")
+p.begin_fill()
+p.right(90)
+
+
+
+h.end_fill()
+
+
+
+
+
+
