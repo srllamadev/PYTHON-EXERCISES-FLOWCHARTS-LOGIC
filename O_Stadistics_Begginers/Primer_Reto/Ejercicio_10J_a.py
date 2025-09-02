2c7469607a53ea168932ff6f176c2e1213b28a19
SrLLamaDev <srllamadev@gmail.com>
2025-09-02 00:28:22 -0400

    Ejercicio 12

---
+++ O_Stadistics_Begginers/Primer_Reto/Ejercicio_10J_a.py
@@ -0,0 +1,14 @@
+# Leer cantidad de números
+N = int(input())
+
+# Inicializar suma de pares
+suma_pares = 0
+
+# Procesar cada número
+for _ in range(N):
+    x = int(input())
+    if x % 2 == 0:
+        suma_pares += x
+
+# Imprimir resultado
+print(suma_pares)
